7ca0632e03c2ae3f6e0669dcbccf1a28731db0c8
Diego Rubio Canales <91208513+DiegoRubiok1@users.noreply.github.com>
2025-02-08 00:11:59 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Created by Diego Rubio Canales in feb 2025
+Universidad Carlos III de Madrid
+"""
+
+#IN PROCESS: Getting the physics and math interpretation of Navier-Stokes
+
+if __name__ == '__main__':
+    pass
