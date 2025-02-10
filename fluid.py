eea8a600a76d8ea1078272d0c4945bb52893d484
Diego Rubio Canales <diegorubiocanales2006@gmail.com>
2025-02-10 19:57:38 +0100

    Commit try

---
+++ fluid.py
@@ -0,0 +1,7 @@
+"""
+Created by Diego Rubio Canales in feb 2025
+Universidad Carlos III de Madrid
+"""
+class Fluid:
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ Created by Diego Rubio Canales in feb 2025
 Universidad Carlos III de Madrid
 """
 
+
 #IN PROCESS: Getting the physics and math interpretation of Navier-Stokes
 
+class Simulation:
+    def __init__(self):
+        pass
+
+
 if __name__ == '__main__':
-    pass
+    simulation = Simulation()
